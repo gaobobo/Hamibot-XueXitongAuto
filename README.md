28644ef0288f7e436d24762c73056f5d6a55bdfb
Shibo Gao <32958058+gaobobo@users.noreply.github.com>
2023-03-02 20:36:06 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,91 @@
-# Hamibot-XueXitongAuto
+<div align="center">
+
+# Hamibot 学习通刷课脚本
+
+<a href="https://hamibot.com/" target="_blank"><img src="./Doc/Resource/WorkWithHamibot.png" alt="导入脚本到 Hamibot" width='200'></img></a> 
+
+**如您是通过`Gitee`平台访问，超链接仍指向速度缓慢的`Github`。**
+
+**我们不会检查`Gitee`内的`Issues`和`Pull Requests`。**
+
+</div>
+
+## 关于此脚本
+
+此脚本可以自动完成课程视频任务点。（不包括练习题）
+
+只适用于Hamibot支持的平台（安卓系统）。
+
+**此代码仅供学习用途使用，这也是为什么我们开源并要求使用此代码的程序开源。**
+
+**关于本代码的使用，详见文末`分发许可`。**
+
+## 如何使用
+
+- 首先，前往[Hamibot](https://hamibot.com/)注册账户。并在安卓设备上安装Hamibot。（[下载Hamibot](https://hamibot.com/download)）
+
+- 打开设备上的Hamibot，并在控制台内[添加机器人](https://hamibot.com/dashboard/robots)。此时，在设备上输入配对码，完成配对。
+
+- 然后，在开发内[创建脚本](https://hamibot.com/dashboard/scripts/create)，在下方上传文件。文件可在本仓库的[Github Release](https://github.com/gaobobo/Hamibot-XueXitongAuto/releases/latest/index.js)下载。
+
+- 之后，确认设备上的Hamibot的无障碍服务开启。
+
+- 最后，前往[开发](https://hamibot.com/dashboard/scripts/console)运行脚本。
+
+- 欲想停止脚本，除了可以在[机器人](https://hamibot.com/dashboard/robots)、[开发](https://hamibot.com/dashboard/scripts/console)内停止外，还可以直接关闭Hamibot。
+
+## 已知问题与反馈
+
+任何的已知问题均可通过Issues提交反馈。
+
+目前的已知问题也罗列在Issues中。
+
+## 帮助完善此项目
+
+欲想共同完善此项目，可通过下面的步骤进行开发。
+
+- 在VS Code中，添加`Hamibot开发助手`扩展
+
+- 克隆本仓库
+
+- 打开克隆后仓库内的`hamibot.config.json`，完善以下字段：
+
+```json
+    "name": "对应Hamibot开发内的脚本名称",
+    "scriptId": "开发-选择对应的脚本-更多-设置-复制ID",
+    "executeRobot": {
+        "_id": "机器人-对应机器人-更多-复制ID",
+        "name": "机器人名称"
+    }
+```
+
+> **注意：此文件内的id为敏感信息。本仓库的Git已经设置提交时忽略此文件。**
+> 
+> **在克隆其他非本仓库的其他副本时，务必检查`.gitignore`文件是否存在并忽略了文件`hamibot.config.json`。**
+>
+> **如使用其他的版本管理工具，请自行设置文件忽略或文件保护。**
+
+- 欲想调试或运行，使用`Ctrl+Shift+P`，搜索`Hamibot`。选择相应操作即可。
+
+## 分发许可
+
+本代码使用`GNU AFFERO GENERAL PUBLIC LICENSE`分发许可。
+
+这意味着：
+
+- 定义网络传播为分发的一种
+
+- 分发时必须提供源代码
+
+- 必须以相同的许可证分发
+
+使用`GNU AFFERO GENERAL PUBLIC LICENSE`分发许可，其目的是让每个人都能平等的访问、使用本代码。
+
+这意味着：
+
+- 本代码可以用于商业用途
+
+- 每个人都有分发和修改代码的权力
+
+- 可以私下使用和修改
+
---
+++ src/index.js
@@ -0,0 +1,343 @@
+/**
+ *              XueXitong Vedio-Courses Automatic Program        
+ *                  Copyright (c) 2023 CoconutFloss
+ * 
+ *           This Hamibot_js could watch course automaticaly.
+ *                         Not including practice.
+ *                  Only for Android Device with Hamibot.
+ *                  Open sorce with The MIT License (MIT).
+ * 
+ *  And:
+ *     The module cannot work when use VS Code. So you may see MANY CLIPS REPEATED.
+ *     I'm used to programming with java, so you may see some not common in js but common in java.
+ * Known Issus(es): 
+ *     When meet same course name but different course author may only finish first one.
+ *     When meet low-speed warning, the program will be unspected.
+ *     When low-speed cause loading, the program maybe unexpected.
+ *     For the point has more than one vedio, the program only finish the first.
+ * 
+ */
+
+function appPackageRetart (appPackageName) {     //restart app
+    app.openAppSetting(appPackageName);
+    sleep(3000);
+    click('强制停止');
+    sleep(3000);
+    click('确定');
+    sleep(3000);
+    app.launch(appPackageName);
+   }
+   function xuexitongToCoursePage () {     //open user profile and course
+       while(!click('我'));
+       while(!click('课程'));
+       sleep(3000);
+   }
+//for check same items vars
+var courseName;
+var courseAuthor;
+var courseEnd;
+function xuexitongGetCoursesList (courseInfo) {   //get courses List
+    var coursesUICollector = id('rl_item').find();
+    coursesUICollector.forEach(function(child){
+
+        var tv_name = child.findOne(id('tv_name'));
+        var tv_subtitile = child.findOne(id('tv_subtitle'));
+
+        if(tv_name==null || tv_subtitile==null) return;
+        if(tv_name.text()==courseName && tv_subtitile.text()==courseAuthor) return;   //skip same item (although the same infact)
+
+        courseName = tv_name.text();
+        courseAuthor = tv_subtitile.text();
+        courseEnd = !child.find(id('tv_end_course_tag')).empty();
+
+        courseInfo.push([courseName,courseAuthor,courseEnd,'0']);
+    });
+    return courseInfo;
+  }
+  function xuexitongGetItemList(itemsList,item){
+    var courseItems = id('sub_node').find();
+
+    if(courseItems.empty()) {   //if items empoty, next for.
+        item[3] = 1;
+        back();
+        sleep(2000);
+        return;
+    }
+
+    courseItems.forEach(function(child){
+        var itemIndex = child.findOne(id('tv_sub_index'));
+        var itemPoint = child.findOne(id('tv_icon'));
+        if (itemIndex == null || itemPoint == null) return;
+        itemsList.push([itemIndex.text(),itemPoint.text()]);
+    });
+  }
+  function xuexitongVedioSelect (choice){
+    // @ts-ignore
+    className('android.widget.ListView').findOnce().children().forEach(function(child){
+        child.children().forEach(function(child){
+            if(child.text()==choice) child.click();
+        })
+    });
+  }
+  function xuexitongVedioAnwser(){
+    if(text('多选题').findOnce() != null) {
+        xuexitongVedioSelect('A');  //abc
+        sleep(1000);
+        xuexitongVedioSelect('B');
+        sleep(1000);
+        xuexitongVedioSelect('C');
+        sleep(1000);
+        click('提交');
+            sleep(1000);
+        if(text('回答错误').findOnce() != null){
+            sleep(1000);
+            xuexitongVedioSelect('A');      //bcd
+            sleep(1000);
+            xuexitongVedioSelect('D');
+            sleep(1000);
+            click('提交');
+            sleep(1000);
+            if(text('回答错误').findOnce() != null){
+                xuexitongVedioSelect('D');      //bc
+                sleep(1000);
+                click('提交');
+                sleep(1000);
+                if(text('回答错误').findOnce() != null){
+                    xuexitongVedioSelect('A');      //ab
+                    sleep(1000);
+                    xuexitongVedioSelect('C');
+                    sleep(1000);
+                    click('提交');
+                    sleep(1000);
+                    if(text('回答错误').findOnce() != null){
+                        xuexitongVedioSelect('B');  //ac
+                        sleep(1000);
+                        xuexitongVedioSelect('C');
+                        sleep(1000);
+                        click('提交');
+                        sleep(1000);
+                        if(text('回答错误').findOnce() != null){
+                            xuexitongVedioSelect('C');      //ad
+                            sleep(1000);
+                            xuexitongVedioSelect('D');
+                            sleep(1000);
+                            click('提交');
+                            sleep(1000);
+                            if(text('回答错误').findOnce() != null){
+                                xuexitongVedioSelect('A');      //bd
+                                sleep(1000);
+                                xuexitongVedioSelect('B');
+                                sleep(1000);
+                                click('提交');
+                                sleep(1000);
+                                if(text('回答错误').findOnce() != null){
+                                    xuexitongVedioSelect('B');      //cd
+                                    sleep(1000);
+                                    xuexitongVedioSelect('C');
+                                    sleep(1000);
+                                    click('提交');
+                                    sleep(1000);
+                                    if(text('回答错误').findOnce() != null){
+                                        back();
+                                        sleep(2000);
+                                        back();
+                                        sleep(2000);
+                                        back();
+                                        sleep(2000);
+                                        return;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        } 
+    }
+    if(text('单选题').findOnce() != null) {
+        xuexitongVedioSelect('A');      //a
+        sleep(1000);
+        click('提交');
+        sleep(1000);
+        if(text('回答错误').findOnce() != null){
+            xuexitongVedioSelect('A');      //b
+            sleep(1000);
+            xuexitongVedioSelect('B');
+            sleep(1000);
+            click('提交');
+            sleep(1000);
+            if(text('回答错误').findOnce() != null){
+                xuexitongVedioSelect('B');      //c
+                sleep(1000);
+                xuexitongVedioSelect('C');
+                sleep(1000);
+                click('提交');
+                sleep(1000);
+                if(text('回答错误').findOnce() != null){
+                    xuexitongVedioSelect('C');      //d
+                    sleep(1000);
+                    xuexitongVedioSelect('D');
+                    sleep(1000);
+                    click('提交');
+                    sleep(1000);
+                    if(text('回答错误').findOnce() != null){
+                        back();
+                        sleep(2000);
+                        back();
+                        sleep(2000);
+                        back();
+                        sleep(2000);
+                        return;
+                    }
+                }
+            }
+        }
+    }
+  }
+  //const SELECT = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+
+auto.waitFor();
+
+
+var appPackageName = 'com.chaoxing.mobile';
+
+//appPackageRetart(appPackageName);
+sleep(15000);
+xuexitongToCoursePage();
+
+
+var courseInfo = new Array();
+do
+{
+    xuexitongGetCoursesList(courseInfo);
+    scrollDown(0);
+    sleep(3000); 
+
+}while(text('已经到底了').find().empty());
+
+xuexitongGetCoursesList(courseInfo);    //unknown reason, the last page won't get info. So repeat it.
+
+
+//appPackageRetart(appPackageName);
+//xuexitongToCoursePage();
+back();     //reset page location
+sleep(3000);
+click('课程');
+
+
+courseInfo.forEach(function(item){
+    if (item[2] == true && item[3] == 1) return;    //check if end or finish
+
+    sleep(3000);
+    while(!click(item[0])){
+        scrollDown(0);
+        sleep(3000);
+    }
+
+    sleep(3000);
+    click('我已阅读，开始学习');    //jump info
+    while(click('取消'));
+    while(!click('章节',0));
+
+    var courseFinishedUIObject = id('tv_job_unfinish_count').findOnce();
+    var courseTotalUIObject = id('tv_job_total_count').findOnce();
+
+    if (courseFinishedUIObject != null && courseTotalUIObject != null) {    //check percent if it has.
+        var courseFinished = courseFinishedUIObject.text();
+        var courseTotal = courseTotalUIObject.text().slice(3);
+    
+        if (courseFinished == courseTotal) {
+            item[3] = 1;
+            back();
+            sleep(2000);
+            return;
+        }
+    }
+
+    var itemsList = new Array();
+
+    /*
+    var courseItems = id('sub_node').find();
+
+    if(courseItems.empty()) {   //if items empoty, next for.
+        item[3] = 1;
+        back();
+        sleep(2000);
+        return;
+    }
+
+    courseItems.forEach(function(child){
+        var itemIndex = child.findOne(id('tv_sub_index'));
+        var itemPoint = child.findOne(id('tv_icon'));
+        if (itemIndex == null || itemPoint == null) return;
+        itemsList.push([itemIndex.text(),itemPoint.text()]);
+    });
+    */
+
+    do {
+
+        xuexitongGetItemList(itemsList,item);
+        scrollDown(0);
+        sleep(3000);
+
+    } while (text('已经到底了').find().empty());
+
+    xuexitongGetItemList(itemsList,item);
+
+    back();
+    sleep(3000);
+    while(!click(item[0]));
+    sleep(3000);
+
+    itemsList.forEach(function(index){
+        if (index[1] == '') return;
+
+        while(!click(index[0])){
+        //while(!click('10.1')){
+            scrollDown(0);
+            sleep(3000);
+        };
+
+        sleep(3000);
+        text('视频').find().forEach(function(child){
+            // @ts-ignore
+            if (child.parent().className() == 'android.support.v7.app.ActionBar$Tab') child.parent().click();
+        })
+        sleep(3000);
+
+        // @ts-ignore
+        if(!text('任务点已完成').find().empty()) {
+            item[1] = '';
+            back();
+            sleep(2000);
+            return;
+        }
+
+        var player = text('播放').findOnce();
+        // @ts-ignore
+        if (player != null) {
+            // @ts-ignore
+            player.findOne(className('Button')).click()
+            sleep(15000);
+            while(id('start').findOnce() == null) {
+                click('重试');
+                xuexitongVedioAnwser();
+                sleep(15000);
+            }
+        }else{
+            item[1] = '';
+            back();
+            sleep(2000);
+            return;
+        }
+
+    item[1] = '';
+    back();
+    sleep(2000);
+    back();
+    sleep(2000);
+    })
+    back();
+    sleep(2000);
+});
